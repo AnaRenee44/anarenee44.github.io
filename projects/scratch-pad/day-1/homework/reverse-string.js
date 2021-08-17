e4c3c9f183c6e82dc7ac85015f44e4fc50eba7db
AnaRenee44 <anarenee9696@gmail.com>
2021-08-17 18:41:00 +0000

    day1 exercises

---
+++ projects/scratch-pad/day-1/homework/reverse-string.js
@@ -0,0 +1,55 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * 1: reverseString()
+ *
+ *  a. Create a function called reverseString that takes a 
+ *  String as its only input and returns a new String 
+ *  representing the input String reversed.  For example:
+ *  
+ *      reverseString('hello');  // => 'olleh'
+ * 
+ * TIPS:
+ *  a. create something to collect the output you'll return.
+ *  b. you'll need a loop, which one is best? How do you know 
+ *     when to stop looping?
+ *  c. how do you access individual characters of a String?
+ *  d. how do you concatenate Strings? What operator do we use?
+ */
+
+function reverseString(input) {
+    // YOUR CODE GOES BELOW HERE //
+    
+    // create an array to contain each input character in reverse
+    var letters = []
+    
+    /*push each characrter from the input string 
+    into the array in reverse */
+    for (var i  = input.length - 1; i > -1; i--) {
+      letters.push(input[i]);
+    }
+    
+    // join the characters listed in the letters array & store them
+    var output = letters.join("");
+    
+    //return the output
+    return output;
+   
+    
+    
+    // YOUR CODE GOES ABOVE HERE //
+}
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if (
+  typeof process !== "undefined" &&
+  typeof process.versions.node !== "undefined"
+) {
+  // here, export any references you need for tests //
+  module.exports.reverseString = reverseString;
+}
---
+++ projects/scratch-pad/day-1/stringy.js
@@ -0,0 +1,244 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: stringy.js
+ */
+
+/**
+ * Given an input String, return its length.
+ *
+ * TIP: In JavaScript, how can we decipher the length of a String?
+ * work?
+ */
+function length(string) {
+    // YOUR CODE BELOW HERE //
+    
+//return length of a given string using .length    
+return string.length;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to lowercase.
+ */
+function toLowerCase(string) {
+    // YOUR CODE BELOW HERE //
+
+//force input to lowercase using reutrn & .toLowerCase()
+return string.toLowerCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to uppercase.
+ */
+function toUpperCase(string) {
+    // YOUR CODE BELOW HERE //
+
+//  return input forced to upper case using return & .toUpperCase()
+return string.toUpperCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to dash-case.
+ *
+ * Examples:
+ *
+ *      toDashCase('Hello World'); // => 'hello-world'
+ *
+ * TIP: How can you look for and replace a space in a String? How can you
+ *      enforce lowercase? Look at the global replace link below, or, try String
+ *      methods split and join?
+ *
+ *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
+ */
+function toDashCase(string) {
+    // YOUR CODE BELOW HERE //
+
+// replace all spaces in the input string with a dash
+let lwrCase = string.toLowerCase();
+return lwrCase.replace(/[" "]/g, "-");
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * begins with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      beginsWith('Max', 'm'); // => true;
+ *      beginsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function beginsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    
+    //set both inputs to the same case
+    var capStr = string.toUpperCase();
+    var capChar = char.toUpperCase();
+    
+/* use if statement to see if char matches 
+the first character of string 
+*/
+    if (capStr[0] == capChar) {
+        return true;
+    } else {
+        return false;
+    }
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * ends with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      endsWith('Max', 'X'); // => true;
+ *      endsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function endsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+
+//set both inputs to the same case
+    var lwrString = string.toLowerCase();
+    var lwrChar = char.toLowerCase();
+/* Use if statement to see if last index of string
+matches index*/
+    if (lwrString[lwrString.length - 1] == lwrChar) {
+        return true;
+    } else {
+        return  false;
+    }
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two input Strings, return the Strings concatenated into one.
+ *
+ * TIP: What's the operator to concatenate two Strings?
+ */
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+// use concatenation operator to join the strings
+return stringOne + stringTwo
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given any number of Strings, return all of them joined together.
+ *
+ * Example:
+ *
+ *      join("my", "name", "is", "Ben"); // => "mynameisBen";
+ *
+ * TIP: This Function pulls out all the arguments passed to it and stores them
+ *      in an Array called args.
+ */
+function join(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    var args = Array.from(arguments);
+    /**
+     * Use join method to return all input strings
+     * passed to the array args concatenated
+     */
+     return args.join("")
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return the longest of the two.
+ *
+ * Example:
+ *
+ *      longest("ben", "maggie");   //-> "maggie"
+ *
+ * TIP: What property of the String do we need to compare?
+ */
+function longest(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+/* Use if statement to compare the lengths of the
+ *  input strings and return the longest
+ */
+    if (stringOne.length > stringTwo.length) {
+        return stringOne;
+    } else if ( stringOne.length < stringTwo.length) {
+        return stringTwo;
+    } else {
+        return "Equal"
+    }
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return 1 if the first is higher in alphabetical order than
+ * the second, return -1 if the second is higher in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortAscending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+//use localeCompare() method to compare strings alphabetically
+return stringTwo.localeCompare(stringOne);
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+/**
+ * Given two Strings, return 1 if the first is lower in alphabetical order than
+ * the second, return -1 if the second is lower in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortDescending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+//use localeCOmpare() method to compare strings alphabetically
+return stringOne.localeCompare(stringTwo)
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.length = length;
+    module.exports.toLowerCase = toLowerCase;
+    module.exports.toUpperCase = toUpperCase;
+    module.exports.toDashCase = toDashCase;
+    module.exports.beginsWith = beginsWith;
+    module.exports.endsWith = endsWith;
+    module.exports.concat = concat;
+    module.exports.join = join;
+    module.exports.longest = longest;
+    module.exports.sortAscending = sortAscending
+    module.exports.sortDescending = sortDescending;
+}
